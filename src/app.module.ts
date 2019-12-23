e3ab92c7bf69cb522fd7a7562b4b5b67e490305f
Zhark10 <arkady.zharavin@citronium.biz>
2019-12-24 00:20:31 +0300

    added module and service for blog

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { BlogModule } from './blog/blog.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/nest-blog', { useNewUrlParser: true }),
+    BlogModule,
   ],
   controllers: [AppController],
   providers: [AppService],
