005d9e75190a253746a236ba1bf2766faa22aa88
Zhark10 <arkady.zharavin@citronium.biz>
2019-12-24 01:03:30 +0300

    refreshed blog module

--- src/blog/blog.module.ts
+++ src/blog/blog.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { BlogService } from './blog.service';
 import { BlogController } from './blog.controller';
+import { BlogService } from './blog.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BlogSchema } from './schemas/blog.schema';
 
 @Module({
-  providers: [BlogService],
+  imports: [
+    MongooseModule.forFeature([{ name: 'Post', schema: BlogSchema }]),
+ ],
   controllers: [BlogController],
+  providers: [BlogService],
 })
-export class BlogModule {}
+export class BlogModule { }
